54862f4ae0c6e2f4bc28a4d410b7884397cb4c1c
Zhoss <joss.90@mail.ru>
2023-08-04 13:09:09 +1000

    Initial commit

---
+++ src/main/java/pro/sky/career_center_attestation/CareerCenterAttestationApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.career_center_attestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCenterAttestationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCenterAttestationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/career_center_attestation/CareerCenterAttestationApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.career_center_attestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCenterAttestationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
